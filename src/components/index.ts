7f5235713efd3baf990a439e760e72784ddd0dde
Iqbal Nurkholis <iqbal132nurkholis@gmail.com>
2024-02-03 21:23:10 +0700

    update export component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import "./index.scss";
 import BButton from "./button/BButton.vue";
 import BPagination from "./pagination/BPagination.vue";
 import BInputRadio from "./inputRadio/BInputRadio.vue";
+import BMultiSelect from "./multiselect/BMultiSelect.vue";
+import BModal from "./modal/BModal.vue";
 
-export { BButton, BPagination, BInputRadio };
+export { BButton, BModal, BPagination, BInputRadio, BMultiSelect };
