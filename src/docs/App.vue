a25d1c457c0c6f55fcfa1c86e43d8e5bbc1b5ca5
Iqbal Nurkholis <iqbal132nurkholis@gmail.com>
2024-02-05 13:53:32 +0700

    update style
    make font poppins global

--- src/docs/App.vue
+++ src/docs/App.vue
@@ -66,17 +66,16 @@ const showModal = () => modal.value?.open()
   <br>
   <br>
   <br>
-  <!-- <BMultiSelect
+  <BMultiSelect
     v-model="multiSelect"
     label="Pilihan"
     validation-text="Validation Text"
     :options="options"
-    disabled
   />
   <BPagination
     v-model="pagination"
     label="Display Grid"
-  /> -->
+  />
 
   <BButton
     text="text"
@@ -112,11 +111,3 @@ const showModal = () => modal.value?.open()
     </template>
   </BButton> -->
 </template>
-
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-body {
-  font-family: 'Poppins', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-</style>
